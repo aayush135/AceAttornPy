d2859e09401c787d85d54686d407974c6eb5b291
aayush135 <singh.asnk@gmail.com>
2025-05-27 21:38:06 +0530

    changed port from 8000 to 80

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# python manage.py runserver 0.0.0.0:8000
-gunicorn aceattorn.wsgi:application --bind 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:80
+gunicorn aceattorn.wsgi:application --bind 0.0.0.0:80
